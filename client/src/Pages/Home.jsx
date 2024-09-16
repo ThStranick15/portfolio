37b6ab82c0d887dba518cfe9a9f61eb85aab9f3d
ThStranick <ThStranick@gmail.com>
2024-09-16 08:35:47 -0400

    improvements

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home(){
             </div>
             <div className="intro">
                 <h1>Hi, welcome to my portfolio!</h1>
-                <h1>My name is Thomas Stranick and I am a Full-Stack Web Developer.</h1>
+                <h1>My name is Thomas Stranick and I am a Software Developer.</h1>
                 <h1>I recently completed the Rutgers Full Stack Web Development Bootcamp and am a graduate from Lafayette College with a major in Electrical and Computer Engineering.</h1>
                 <h1>Please take a look around my site and don't hesitate to reach out!</h1>
                 <NavLink to="/portfolio">Check out my Portfolio &#8594;</NavLink>
