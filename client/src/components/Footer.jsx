4137a119792716c308a56fb22a923c897f625447
ThStranick <ThStranick@gmail.com>
2025-03-28 08:14:58 -0400

    updating

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,14 +1,14 @@
 export default function Footer(){
     return(
         <footer>
-            <p className="copy">&copy; Copyright 2024</p>
+            <p className="copy">&copy; Copyright 2025</p>
             <div className="socials">
                 <a href="https://github.com/ThStranick15">GitHub</a>
                 <a href="https://www.linkedin.com/in/thomas-stranick/">Linkedin</a>
             </div>
             <p className="created">Created by: Thomas Stranick</p>
             <div className="mobile-footer">
-                <p>&copy; Copyright 2024</p>
+                <p>&copy; Copyright 2025</p>
                 <p>Created by: Thomas Stranick</p>
             </div>
         </footer>
