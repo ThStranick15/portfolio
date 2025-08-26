33e3a0bfc0ad8cccae8d9ed8c15d295811ebc784
ThStranick <ThStranick@gmail.com>
2025-08-26 07:59:10 -0400

    added fantasy

--- client/src/Pages/Portfolio/index.jsx
+++ client/src/Pages/Portfolio/index.jsx
@@ -68,6 +68,7 @@ export default function Portfolio(){
                 <h2>Parksinson's Detection Using Handwriting Samples - <a href="/FSRM588 Group-5-Final-1.pdf">View Report</a></h2>
                 <h2>Student Growth Mindset Observational Study - <a href="/Stranick_SMC_Poster.pdf">View Report</a></h2>
                 <h2>Movie Trailer Sentiment Analysis - <a href="/Guo_Stranick_Project_Report.pdf">View Report</a></h2>
+                <h2>Fantasy Football Points Analysis - <a href="/Fantasy.pdf">View Report</a></h2>
             </div>
         </section>
         <section className="portfolio">
